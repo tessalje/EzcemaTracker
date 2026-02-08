09acb6240972be969ca554cf7d1185eaa7d0d4db
tessalje <assetessa77@gmail.com>
2026-02-09 00:09:55 +0800

    Initial Commit

---
+++ EzcemaTracker/EzcemaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzcemaTrackerApp.swift
+//  EzcemaTracker
+//
+//  Created by Tessa Lee on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct EzcemaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EzcemaTrackerTests/EzcemaTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EzcemaTrackerTests.swift
+//  EzcemaTrackerTests
+//
+//  Created by Tessa Lee on 9/2/26.
+//
+
+import Testing
+@testable import EzcemaTracker
+
+struct EzcemaTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
